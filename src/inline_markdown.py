7cb4a2362c13097465956d47e9b228877a10aa48
graylum97 <luke.gray97@icloud.com>
2026-02-02 22:09:56 -0600

    second commit

---
+++ src/inline_markdown.py
@@ -0,0 +1,102 @@
+import re
+from src.textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+
+        if delimiter not in text:
+            new_nodes.append(node)
+            continue
+
+        parts = text.split(delimiter)
+
+        if len(parts) % 2 == 0:
+            raise Exception("Invalid markdown, unmatched delimiter")
+
+        for i, part in enumerate(parts):
+            if part == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(part, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(part, text_type))
+
+    return new_nodes
+
+def extract_markdown_images(text: str):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_links(text: str):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        images = extract_markdown_images(node.text)
+
+        if len(images) == 0:
+            new_nodes.append(node)
+            continue
+
+        remaining_text = node.text
+        for image in images:
+            sections = remaining_text.split(f"![{image[0]}]({image[1]})", 1)
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT)
+
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+
+            remaining_text = sections[1]
+
+        if remaining_text != "":
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        links = extract_markdown_links(node.text)
+
+        if len(links) == 0:
+            new_nodes.append(node)
+            continue
+
+        remaining_text = node.text
+        for link in links:
+            sections = remaining_text.split(f"[{link[0]}]({link[1]})", 1)
+
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+
+            remaining_text = sections[1]
+
+        if remaining_text != "":
+            new_nodes.append(TextNode(remaining_text, TextType.TEXT))
+
+    return new_nodes
